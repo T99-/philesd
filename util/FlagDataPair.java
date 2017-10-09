79e7a3b3cea1fead48533ae32a84e70349584e6e
Trevor Sears <trevorsears.main@gmail.com>
2017-10-09 15:54:33 -0400

    Started work on CLI handler.

---
+++ util/FlagDataPair.java
@@ -0,0 +1,21 @@
+package com.t99sdevelopment.philesd.util;
+
+public class FlagDataPair {
+	
+	public String primary;
+	public String secondary;
+	
+	public FlagDataPair(String primary, String secondary) {
+		
+		this.primary = primary;
+		this.secondary = secondary;
+		
+	}
+	
+	public FlagDataPair() {
+	
+	
+	
+	}
+	
+}
